8e495d86d1d251e8e22e7851b52baea01620fe36
Covasc <lucas.rodmarques@gmail.com>
2022-07-20 15:58:52 -0300

    MVC Architecture

--- src/db.js
+++ src/db.js
@@ -4,12 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT;
-let db;
 
 //DB CONECTION
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-mongoClient.connect().then(() => {
-    db = mongoClient.db("myWallet");
-})
+await mongoClient.connect();
+const db = mongoClient.db("myWallet");
 
 export default db;
